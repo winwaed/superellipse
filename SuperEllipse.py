2b9c5de2ac509cdd66a6cce3e871062f060d7e0d
Richard Marsden <richard@winwaed.com>
2019-08-01 10:28:51 -0500

    Update SuperEllipse.py
    explicit bsd

--- SuperEllipse.py
+++ SuperEllipse.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
-""" SuperEllipse Drawing in SVG
+# Copyright 2019 Winwaed Software Technology LLC
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+# conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+# disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-(C) Copyright Richard Marsden, Winwaed Software Technology LLC 2018
-Code is distributed under the Berkeley (BSD) '3 clause' license
+""" SuperEllipse Drawing to an SVG file
+
+See the se_demo.py for example usage
 """
-
 
 from math import pi,cos,sin,pow,fabs,copysign
 import os
